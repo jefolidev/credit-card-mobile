1201452a2a39ce66d52a3ec8432ec9e01e616c3d
jefolidev <jefoliveira279@gmail.com>
2025-12-08 15:16:43 -0300

    feat: integrate CashAmount component in Home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
+import { CashAmount } from 'src/components/cash-amount'
 import { CreditCard } from 'src/components/credit-card'
-import CreditCartIcon from '../../assets/credit-card'
+import CreditCardIcon from '../../assets/credit-card'
 import { Header } from '../../components/header'
 import colors from '../../theme/colors'
 
@@ -12,7 +13,7 @@ export function Home() {
       <Header
         title="Titulo foda"
         icon={
-          <CreditCartIcon width={22} height={22} strokeColor={primaryText} />
+          <CreditCardIcon width={22} height={22} strokeColor={primaryText} />
         }
       />
       <CreditCard
@@ -20,6 +21,7 @@ export function Home() {
         cardOwner="Cleiton Campelo da Silva"
         cardVality="04/2020"
       />
+      <CashAmount />
     </View>
   )
 }
